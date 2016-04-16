7741543231a6bbbe0d56149ff90253bb0cc246d8
LazyCat <ds3dxf@hanmail.net>
2016-04-16 13:37:33 +0000

    add footer, modify little

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,19 @@
 class WelcomeController < ApplicationController
   def index
   end
+  
+  def about
+  end
+  
+  def rank_tutor
+  end
+  
+  def rank_tutor_in
+  end
+  
+  def search
+  end
+  
+  def search_in
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'welcome/about'
+  get 'welcome/rank_tutor'
+  get 'welcome/rank_tutor_in'
+  get 'welcome/search'
+  get 'welcome/search_in'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
