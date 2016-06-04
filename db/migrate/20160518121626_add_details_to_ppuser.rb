23845db9908a6941f36bab493bc3f4a47af8c223
seongahkim <ksa0356@gmail.com>
2016-06-04 16:39:07 +0000

    아이엠씨티오/회원가입오류해결

--- db/migrate/20160518121626_add_details_to_ppuser.rb
+++ db/migrate/20160518121626_add_details_to_ppuser.rb
@@ -1,10 +1,33 @@
 class AddDetailsToPpuser < ActiveRecord::Migration
-  def change
+  def self.up
     
-    add_column :ppusers, :user_name, :string, null: false
-    add_column :ppusers, :nick_name, :string, null: false
-    add_column :ppusers, :identify_type, :string, null: false
-    add_column :ppusers, :matching_number, :integer, null: false
+    add_column :ppusers, :user_name, :string
+    add_column :ppusers, :nick_name, :string
+    add_column :ppusers, :identify_type, :string
+    add_column :ppusers, :matching_number, :integer
+    
+    Ppuser.reset_column_information
+ 
+    Ppuser.all.each do |ppuser|
+      ppuser.user_name = ppuser.created_at
+      ppuser.nick_name = ppuser.created_at
+      ppuser.identify_type = ppuser.created_at
+      ppuser.matching_number = ppuser.created_at
+      ppuser.save!
+    end
+ 
+    change_column :ppusers, :user_name, :string, :null => false
+    change_column :ppusers, :nick_name, :string, :null => false
+    change_column :ppusers, :identify_type, :string, :null => false
+    change_column :ppusers, :matching_number, :string, :null => false
     
   end
+  
+  def self.down
+    remove_column :ppusers, :user_name
+    remove_column :ppusers, :nick_name
+    remove_column :ppusers, :identify_type
+    remove_column :ppusers, :matching_number
+  end
+  
 end
